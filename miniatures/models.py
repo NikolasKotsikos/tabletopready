5b18f9aa45899cd28cb31a1c46117930c213d0e7
NikolasKotsikos <kotsikn@gmail.com>
2021-04-05 16:32:19 +0000

    Added miniatures models.py, removed fixtures

--- miniatures/models.py
+++ miniatures/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    state = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=200)
+    state = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Miniature(models.Model):
+    name = models.CharField(max_length=200)
+    subcategory = models.ForeignKey(Subcategory,
+                                    unique=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, blank=True,
+                                 default=None, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
