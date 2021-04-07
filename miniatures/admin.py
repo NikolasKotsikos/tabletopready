5072dd5043f623845d799208aeb1654bf05fbdb0
NikolasKotsikos <kotsikn@gmail.com>
2021-04-07 14:06:17 +0000

    Changed all media files to jpg, added category and miniature models in miniatures/models.py

--- miniatures/admin.py
+++ miniatures/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Miniature, Category
 
 # Register your models here.
+admin.site.register(Miniature)
+admin.site.register(Category)
--- miniatures/models.py
+++ miniatures/models.py
@@ -2,28 +2,48 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
-    state = models.BooleanField(default=True)
-
+    parent = models.ForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.CASCADE)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    slug = models.SlugField(default=None)
+        
     def __str__(self):
-        return self.name
-
-
-class Subcategory(models.Model):
-    name = models.CharField(max_length=200)
-    state = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+        full_path = [self.name]                  
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' -> '.join(full_path[::-1])
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+    class Meta:
+        # enforcing that there can not be two categories under a parent with the same slug
+        unique_together = ('slug', 'parent',)
+        verbose_name_plural = "categories"
 
 
 class Miniature(models.Model):
-    name = models.CharField(max_length=200)
-    subcategory = models.ForeignKey(Subcategory,
-                                    unique=True, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, blank=True,
-                                 default=None, on_delete=models.CASCADE)
+                                 default=None, on_delete=models.CASCADE)    
+    name = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    date_added = models.DateField(auto_now=False, auto_now_add=True) 
+    slug = models.SlugField(default=None, unique=True)       
 
     def __str__(self):
         return self.name
+    
+    def get_cat_list(self):
+        k = self.category
+        
+        breadcrumb = ["dummy"]
+        while k is not None:
+            breadcrumb.append(k.slug)
+            k = k.parent
+        for i in range(len(breadcrumb)-1):
+            breadcrumb[i] = '/'.join(breadcrumb[-1:i-1:-1])
+        return breadcrumb[-1:0:-1]
