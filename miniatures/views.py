0bb10a9d95414bd77020ad229867fa5c81b8971f
NikolasKotsikos <kotsikn@gmail.com>
2021-04-08 13:13:40 +0000

    Added miniatures views and template

--- miniatures/views.py
+++ miniatures/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Miniature
 
-# Create your views here.
+
+def all_miniatures(request):
+    """ A view to show all miniatures, including sorting and search queries """
+
+    miniatures = Miniature.objects.all()
+
+    context = {
+        'miniatures': miniatures,
+    }
+
+    return render(request, 'miniatures/miniatures.html', context)
