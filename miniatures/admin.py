77aafa5c2ee78cd85c2ef2328dfe5fc505a010df
NikolasKotsikos <kotsikn@gmail.com>
2021-04-08 12:55:25 +0000

    Customized admin

--- miniatures/admin.py
+++ miniatures/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from .models import Miniature, Category
+from .models import Miniature, Manufacturer
+
 
 # Register your models here.
-admin.site.register(Miniature)
-admin.site.register(Category)
+class MiniatureAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'gaming_system',
+        'faction',
+        'price',
+        'date_added',
+        'image',
+    )
+
+    ordering = ('date_added',)
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Miniature, MiniatureAdmin)
+admin.site.register(Manufacturer, ManufacturerAdmin)
--- miniatures/models.py
+++ miniatures/models.py
@@ -1,49 +1,30 @@
 from django.db import models
+from datetime import datetime
 
 
-class Category(models.Model):
-    parent = models.ForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.CASCADE)
+class Manufacturer(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    slug = models.SlugField(default=None)
-        
-    def __str__(self):
-        full_path = [self.name]                  
-        k = self.parent
-        while k is not None:
-            full_path.append(k.name)
-            k = k.parent
-        return ' -> '.join(full_path[::-1])
-    
-    def get_friendly_name(self):
-        return self.friendly_name
-    
-    class Meta:
-        # enforcing that there can not be two categories under a parent with the same slug
-        unique_together = ('slug', 'parent',)
-        verbose_name_plural = "categories"
-
-
-class Miniature(models.Model):
-    category = models.ForeignKey(Category, blank=True,
-                                 default=None, on_delete=models.CASCADE)    
-    name = models.CharField(max_length=254)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-    date_added = models.DateField(auto_now=False, auto_now_add=True) 
-    slug = models.SlugField(default=None, unique=True)       
 
     def __str__(self):
         return self.name
-    
-    def get_cat_list(self):
-        k = self.category
-        
-        breadcrumb = ["dummy"]
-        while k is not None:
-            breadcrumb.append(k.slug)
-            k = k.parent
-        for i in range(len(breadcrumb)-1):
-            breadcrumb[i] = '/'.join(breadcrumb[-1:i-1:-1])
-        return breadcrumb[-1:0:-1]
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Miniature(models.Model):
+    manufacturer = models.ForeignKey('Manufacturer',
+                                 null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    gaming_system = models.CharField(max_length=254)
+    army = models.CharField(max_length=254)
+    faction = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField(default=None)
+    date_added = models.DateTimeField(default=datetime.now, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
