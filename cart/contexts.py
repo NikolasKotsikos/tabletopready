de4cff484c433889d95502009b0451becc7907bd
NikolasKotsikos <kotsikn@gmail.com>
2021-04-11 18:19:39 +0000

    Added table in cart.html

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from miniatures.models import Miniature
 
 
 def cart_contents(request):
@@ -7,6 +9,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     miniatures_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        miniature = get_object_or_404(Miniature, pk=item_id)
+        total += quantity * miniature.price
+        miniatures_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'miniature': miniature,
+        })
 
     if total < settings.FREE_SHIPPING_THRESHOLD:
         shipping = total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE)
