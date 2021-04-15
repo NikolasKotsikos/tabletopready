6f15d64f2bfad13e4429768116395cf89aa5cf2d
NikolasKotsikos <kotsikn@gmail.com>
2021-04-15 14:59:57 +0000

    Created checkout app and models

---
+++ checkout/models.py
@@ -0,0 +1,83 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from miniatures.models import Miniature
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=False, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    shipping_cost = models.DecimalField(max_digits=6,
+                                        decimal_places=2,
+                                        null=False,
+                                        default=0)
+    order_total = models.DecimalField(max_digits=10,
+                                      decimal_places=2,
+                                      null=False,
+                                      default=0)
+    grand_total = models.DecimalField(max_digits=10,
+                                      decimal_places=2,
+                                      null=False,
+                                      default=0)
+
+    def _generate_order_number(self):
+        """
+        Generates a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+ 
+    def update_total(self):
+        """
+        Updates grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total_sum']
+        if self.order_total < settings.FREE_SHIPPING_THRESHOLD:
+            self.shipping_cost = self.order_total * settings.STANDARD_SHIPPING_PERCENTAGE / 100
+        else:
+            self.shipping_cost = 0
+        self.grand_total = self.order_total + self.shipping_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the original save method to set the order number
+        if it hasn't been set already
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    miniature = models.ForeignKey(Miniature, null=False, blank=False, on_delete=models.CASCADE)
+    miniature_faction = models.CharField(max_length=2, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the original save method to set the order number
+        if it hasn't been set already
+        """
+        self.lineitem_total = self.miniature.price * self.quantity
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'SKU {self.miniature.sku} on order {self.order.order_number}'
--- tabletop_ready/settings.py
+++ tabletop_ready/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'miniatures',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
