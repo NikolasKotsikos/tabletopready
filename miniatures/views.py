ef41b1c0b2947c42a0f60d3af2ff6083374567e9
NikolasKotsikos <kotsikn@gmail.com>
2021-04-21 14:56:05 +0000

    Main site components complete

--- miniatures/views.py
+++ miniatures/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-from itertools import chain
 
 from .models import Miniature, GamingSystem, Army
 from .forms import MiniatureForm, ArmyForm, GameSystemForm
