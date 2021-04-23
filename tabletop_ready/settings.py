060f9e086159dce9cff703a8bd083f13c82f13be
NikolasKotsikos <kotsikn@gmail.com>
2021-04-23 05:13:30 +0000

    Removed secret key and set debug

--- tabletop_ready/settings.py
+++ tabletop_ready/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=5&o-!!vx=xtv+vwu*rm5(2y&r==605z6_spv)5-tq+$5y-25n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tabletop-ready.herokuapp.com', 'localhost']
 
