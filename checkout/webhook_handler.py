2d6db6032eacb10c6394633d42b9eedca801bf75
NikolasKotsikos <kotsikn@gmail.com>
2021-06-22 13:34:57 +0000

    Added comments in HTML code

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -12,7 +12,9 @@ import time
 
 
 class StripeWH_Handler:
-    """Handle Stripe webhooks"""
+    """
+    Handle Stripe webhooks    
+    """
 
     def __init__(self, request):
         self.request = request
